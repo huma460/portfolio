97e0e8f772e6de1cb9e3bd65cd285de7ff39bb96
huma460 <mr.doom0077@gmail.com>
2026-06-01 11:09:20 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Smooth scroll for navigation
-document.querySelectorAll('.links a').forEach(link => {
-  link.addEventListener('click', e => {
-    e.preventDefault();
-    document.querySelector(link.getAttribute('href')).scrollIntoView({ 
-      behavior: 'smooth' 
-    });
-  });
-});
+function showMessage(){
+
+alert(
+"Thank you for visiting Huma Noor's Portfolio Website!"
+);
+
+}
